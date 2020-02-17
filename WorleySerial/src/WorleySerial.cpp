6b03129c920bdcd0f66ccf3335a7ff9cba7c942a
gertab <gerardtabone@yahoo.com>
2020-02-17 13:43:52 +0100

    Improved random point generation

--- WorleySerial/src/WorleySerial.cpp
+++ WorleySerial/src/WorleySerial.cpp
@@ -10,93 +10,82 @@
 #include "shared/jbutil.h"
 
 
-int normDistanceFromNearestPoint(int x, int y, float width, float height, std::vector<std::vector<int>> random_points) {
+int normDistanceFromNearestPoint(int x, int y, float width, float height, int *random_points, int N) {
 
-	int shortest_norm_dist = 100;
+int shortest_norm_dist = 100;
 
-	for(unsigned int i = 0; i < random_points.size(); i++) {
-		int x_point = random_points.at(i).at(0);
-		int y_point = random_points.at(i).at(1);
-		float x_dist = (x - x_point);
-		float y_dist = (y - y_point);
+for(unsigned int i = 0; i < N; i++) {
+	float x_point = *(random_points + i * 2);
+	float y_point = *((random_points + i * 2) + 1);
+	float x_dist = (x - x_point) / 2.0;
+	float y_dist = (y - y_point) / 2.0;
 
-		int distance = sqrt(x_dist * x_dist + y_dist * y_dist);
+	int distance = sqrt(x_dist * x_dist + y_dist * y_dist);
 
-		shortest_norm_dist = std::min(distance, shortest_norm_dist);
-	}
-
-	return shortest_norm_dist;
+	shortest_norm_dist = std::min(distance, shortest_norm_dist);
 }
 
-//def _get_normalized_distance_from_nearest_point(pixel_x: int,
-//                                                pixel_y: int,
-//                                                img_width: int,
-//                                                img_height: int,
-//                                                random_points: list):
-//    shortest_norm_dist = 1
-//    for point_x, point_y in random_points:
+return shortest_norm_dist;
+}
+
 //        x_dist = (pixel_x - point_x) / (img_width / 4)
 //        y_dist = (pixel_y - point_y) / (img_height / 4)
 //        norm_dist = math.sqrt(x_dist ** 2 + y_dist ** 2)
-//
-//        shortest_norm_dist = min(norm_dist, shortest_norm_dist)
-//
-//    return shortest_norm_dist
 
 template <class real>
 void process(const std::string infile, const std::string outfile,
-      const real R, const int a)
-   {
-   // start timer
-   double t = jbutil::gettime();
+  const real R, const int a)
+{
+	// start timer
+	double t = jbutil::gettime();
 
 
-   int height = 1000, width = 1000;
+	int height = 2000, width = 2000;
+	int N = 200;
+	int seed = 5;
+
+	jbutil::randgen rand(seed);
+
+	int random_points[N][2];
 
 
-	std::vector<std::vector<int>> random_points = std::vector<std::vector<int>>();
-
-
-	jbutil::randgen rand(4);
-
-	for(int i = 0; i <  100; i++) {
+	for(int i = 0; i <  N; i++) {
 		rand.advance();
-
-		int x = (int) rand.fval(0, width), y = (int) rand.fval(0, width);
-		random_points.push_back({x, y});
+		random_points[i][0] = (int) rand.fval(0, width);
+		rand.advance();
+		random_points[i][1] = (int) rand.fval(0, height);
 
 	}
 
-   // TODO: Implemented image resampling here
-   jbutil::image<int> image_out = jbutil::image<int>(height, width, 1, 255);
+	jbutil::image<int> image_out = jbutil::image<int>(height, width, 1, 255);
 
-   for(int i = 0; i < height; i++) {
+	for(int i = 0; i < height; i++) {
 	   for(int j = 0; j < width; j++) {
-		   image_out(0, i, j) = normDistanceFromNearestPoint(j, i, width, height, random_points)  % 255;
+		   image_out(0, i, j) = normDistanceFromNearestPoint(j, i, width, height, (int *) random_points, N)  % 255;
 	   }
-   }
+	}
 
-   // stop timer
-   t = jbutil::gettime() - t;
-   // save image
-   std::ofstream file_out(outfile.c_str());
-   image_out.save(file_out);
-   // show time taken
-   std::cerr << "Time taken: " << t << "s" << std::endl;
-   }
+	// stop timer
+	t = jbutil::gettime() - t;
+	// save image
+	std::ofstream file_out(outfile.c_str());
+	image_out.save(file_out);
+	// show time taken
+	std::cerr << "Time taken: " << t << "s" << std::endl;
+}
 
 // Main program entry point
 
 int main(int argc, char *argv[])
 {
-//	std::cerr << "Lab 2: Image resampling with Lanczos filter" << std::endl;
-//	if (argc != 5)
-//	{
-//		std::cerr << "Usage: " << argv[0]
-//		<< " <infile> <outfile> <scale-factor> <limit>" << std::endl;
-//		exit(1);
-//	}
-//	process<float> (argv[1], argv[2], atof(argv[3]), atoi(argv[4]));
+	//	std::cerr << "Lab 2: Image resampling with Lanczos filter" << std::endl;
+	//	if (argc != 5)
+	//	{
+	//		std::cerr << "Usage: " << argv[0]
+	//		<< " <infile> <outfile> <scale-factor> <limit>" << std::endl;
+	//		exit(1);
+	//	}
+	//	process<float> (argv[1], argv[2], atof(argv[3]), atoi(argv[4]));
 	process<float> ("", "out", 0.1, 1);
 
 
